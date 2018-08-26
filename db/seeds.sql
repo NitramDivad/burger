f29949d09fb4001785f5450496861669fb33eb22
MBP 2017 <38024526+NitramDivad@users.noreply.github.com>
2018-08-25 22:28:46 -0600

    component updates

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder with Cheese');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
